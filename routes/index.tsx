e3f9a18e54cc89666fe1a88ae8708778e9f0e21a
lucca <luccameds@gmail.com>
2023-11-03 20:26:27 -0300

    Setup: Configured the firebase on the project.

---
+++ routes/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AuthorizedRoutes = () => {
+  return (
+    <View>
+      <Text>AuthorizedRoutes</Text>
+    </View>
+  );
+};
+
+const UnauthorizedRoutes = () => {
+  return (
+    <View>
+      <Text>UnauthorizedRoutes</Text>
+    </View>
+  );
+};
+
+const Routes = () => {
+  return (
+    <View>
+      <Text>Routes</Text>
+    </View>
+  );
+};
+
+export default Routes;
+
+const styles = StyleSheet.create({});
