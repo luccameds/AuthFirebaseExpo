ad887d867f0cfe75200445ac3e864d13f9d9a293
lucca <luccameds@gmail.com>
2023-11-06 20:45:44 -0300

    Feat: Tela de SignOut criada.

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '../contexts/authContext';
 
 const Home = () => {
+  const { authContext } = useAuth();
+
   return (
     <View>
-      <Text>Tela Autorizada</Text>
+      <TouchableOpacity
+        onPress={() => {
+          authContext.signOut();
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Deslogar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#d90202',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+});
